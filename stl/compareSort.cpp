2d9b7b21253cfba67829aed95fd118db3e7616ed
Manish Kumar <manishju22@gmail.com>
2020-10-31 11:35:13 +0530

    Add stl

---
+++ stl/compareSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool compare(int a,int b)
+{
+    // descending order
+    return a>b;
+}
+void solve()
+{
+   int arr[] ={1,5,6,3,4,5,3,9};
+   int n=sizeof(arr)/sizeof(int);
+   
+   //sort according to compare function
+   sort(arr,arr+n,compare);
+
+   for(int i=0;i<n;i++)
+   cout<<arr[i]<<" ";
+   cout<<"\n";
+
+   /*
+      Output
+      ----------
+       9 6 5 5 4 3 3 1 
+   */
+
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	solve();
+	return 0;
+}
